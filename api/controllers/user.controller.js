15993e0cb3d08f14ea8bc5b38ba5fdd0486a4c98
Ravi Teja <geddadaraviteja612@gmail.com>
2024-07-28 15:24:36 -0400

    add contact landlord functionality to the listing page

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -47,7 +47,6 @@ export const deleteUser = async (req, res, next) => {
 }
 
 export const getUserListings = async (req, res, next) => {
-
     if(req.user.id === req.params.id) {
         try {
             const listing = await Listing.find({ userRef: req.params.id });
@@ -57,6 +56,21 @@ export const getUserListings = async (req, res, next) => {
         }
     } else {
         return next(errorHandler(401, 'You can only view your own listings!'));
+    }
+
+}
+
+export const getUser = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.params.id);
+    
+        if(!user) return next(errorHandler(404, 'User not found'));
+    
+        const { password: pass, ...rest } = user._doc;
+    
+        res.status(200).json(rest);
+    } catch(error) {
+        next(error);
     }
 
 }
--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -13,6 +13,9 @@ import {
   FaParking,
   FaShare,
 } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import { getUser } from "../helpers/selectors";
+import Contact from "../components/Contact";
 
 // https://sabe.io/blog/javascript-format-numbers-commas#:~:text=The%20best%20way%20to%20format,format%20the%20number%20with%20commas.
 
@@ -22,7 +25,9 @@ export default function Listing() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [copied, setCopied] = useState(false);
+  const [contact, setContact] = useState(false);
   const params = useParams();
+  const { currentUser } = useSelector(getUser);
   useEffect(() => {
     const fetchListing = async () => {
       try {
@@ -131,6 +136,10 @@ export default function Listing() {
                 {listing.furnished ? "Furnished" : "Unfurnished"}
               </li>
             </ul>
+            {currentUser && listing.userRef !== currentUser._id && !contact && (
+                <button onClick={() => setContact(true)} className="bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3">Contact Landloard</button>
+            )}
+            {contact && <Contact listing={listing} />}
           </div>
         </>
       )}
