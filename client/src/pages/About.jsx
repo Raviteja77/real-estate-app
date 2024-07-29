f9dc7b23cb768b6bb1da7c2001612aade930f9fb
Ravi Teja <geddadaraviteja612@gmail.com>
2024-07-29 18:52:35 -0400

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About BP Requirements</h1>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, perferendis at labore corrupti consequatur magnam sequi, alias molestiae necessitatibus illum voluptatum officiis inventore quasi, eius dolorum veritatis maxime nihil ex!
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut corrupti nobis eligendi molestias placeat maxime at, molestiae nihil quisquam consequatur voluptatibus saepe aut sequi rem quibusdam pariatur exercitationem sunt hic?
+      </p>
     </div>
   )
 }
